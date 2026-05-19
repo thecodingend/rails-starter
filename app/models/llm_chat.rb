fcc641f48c7783b02dd28d579cab16472947f1bc
Nazar Khasanov <nazar.hasanov@bt.com>
2026-05-19 07:54:50 +0100

    fix: failing pipeline

--- app/models/llm_chat.rb
+++ app/models/llm_chat.rb
@@ -1,6 +1,6 @@
 class LlmChat < ApplicationRecord
-  # rubyllm's acts_as_chat must be included after the model declaration, Its purpose is to set up the necessary associations and methods for the chat functionality. 
-  # messages: :llm_messages specifies that the chat messages are stored in the llm_messages association, 
+  # rubyllm's acts_as_chat must be included after the model declaration, Its purpose is to set up the necessary associations and methods for the chat functionality.
+  # messages: :llm_messages specifies that the chat messages are stored in the llm_messages association,
   # model: :llm_model indicates that the chat uses the llm_model for its language model interactions.
   acts_as_chat messages: :llm_messages, model: :llm_model
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-
   # Redirect to localhost from 127.0.0.1 to use same IP address with Vite server
   constraints(host: "127.0.0.1") do
     get "(*path)", to: redirect { |params, req| "#{req.protocol}localhost:#{req.port}/#{params[:path]}" }
   end
-  root 'inertia_example#index'
-  get 'inertia-example', to: 'inertia_example#index'
+  root "inertia_example#index"
+  get "inertia-example", to: "inertia_example#index"
   get "agent-playground", to: "agent_playground#index", as: :agent_playground
   post "agent-playground", to: "agent_playground#create"
   namespace :internal do
